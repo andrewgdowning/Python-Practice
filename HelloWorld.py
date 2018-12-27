837970d491c44522fe44a1db26f5a53ec70d49d8
Andrew Downing <a_downing@hotmail.com>
2018-12-27 11:44:22 +0000

    Added first variable

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,9 @@
 print('Hello Python World!')
 print('This is Andrew Downings first Python Project')
 print('We are now using git to control our work!')
+print('Now we are testing with github')
+
+github_variable = Hello
+
+
+print(github_variable)
