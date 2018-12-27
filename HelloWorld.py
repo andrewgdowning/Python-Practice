0b4ea3ec9d609cce7fbd9b5adca87583abad656b
Andrew Downing <a_downing@hotmail.com>
2018-12-27 11:46:32 +0000

    Git commit number 2

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,7 +3,8 @@ print('This is Andrew Downings first Python Project')
 print('We are now using git to control our work!')
 print('Now we are testing with github')
 
-github_variable = Hello
 
 
-print(github_variable)
+it_1 = 1 + 2
+
+print(it_1)
